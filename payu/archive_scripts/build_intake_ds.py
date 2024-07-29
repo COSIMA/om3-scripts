c0528597b31c99930387d299b513f5c653354dc0
anton-seaice <anton.steketee@anu.edu.au>
2024-07-29 17:07:13 +1000

    some review comments

--- payu/archive_scripts/build_intake_ds.py
+++ payu/archive_scripts/build_intake_ds.py
@@ -8,6 +8,7 @@
 #     - conda/analysis
 
 from access_nri_intake.source import builders
+from payu.metadata import Metadata
 import os
 
 from pathlib import Path
@@ -32,14 +33,7 @@ builder.build()
 builder.invalid_assets
 
 # Get experiment uuid
-# follows https://github.com/payu-org/payu/blob/ef55e93fe23fcde19024479c0dc4112dcdf6603f/payu/metadata.py#L90
-metadata_filename = Path(METADATA_FILENAME)
-if metadata_filename.exists():
-    metadata = CommentedMap()
-    metadata = YAML().load(metadata_filename)
-    uuid = metadata.get(UUID_FIELD, None)
-else:
-    warn(f"{METADATA_FILENAME} not found in archive folder")
+uuid = Metadata(laboratory_archive_path="archive/").uuid
 
 # Check git status of this .py file
 this_file = os.path.normpath(__file__)
@@ -49,9 +43,14 @@ runcmd = f"python3 {os.path.basename(this_file)}"
 # Get string "Created using $file: $command"
 provenance = get_provenance_metadata(this_file, runcmd)
 
+if uuid:
+    description = f"intake-esm datastore for experiment {uuid}, in folder {os.getcwd()}. {provenance}. (md5 hash: {md5sum(this_file)})"
+else:
+    description = f"intake-esm datastore for experiment in folder {os.getcwd()}. {provenance}. (md5 hash: {md5sum(this_file)})"
+
 # Save the datastore to a file (json)
 builder.save(
-    name=f"intake_datastore_{uuid[0:8]}",
-    description=f"intake_datastore for experiment {uuid}, in folder {os.getcwd()}. {provenance}. (md5 hash: {md5sum(this_file)})",
+    name=f"intake_esm_ds",
+    description=description,
     directory="archive/",
 )
