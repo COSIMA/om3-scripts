1dd080968c50692fb16c5a739e075e3857b3454e
Anton Steketee <79179784+anton-seaice@users.noreply.github.com>
2024-07-31 19:46:41 +1000

    Update archive.sh

--- payu_config/archive.sh
+++ payu_config/archive.sh
@@ -2,11 +2,4 @@
 
 source $(dirname "$0")/archive_scripts/archive_cice_restarts.sh
 source $(dirname "$0")/archive_scripts/concat_ice_daily.sh
-
-# modules:
-# use:
-#     - /g/data/hh5/public/modules
-# load:
-#     - conda/analysis
-
 python3 $(dirname "$0")/archive_scripts/build_intake_ds.py
