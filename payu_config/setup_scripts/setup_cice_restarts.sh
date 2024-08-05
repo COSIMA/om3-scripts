efb2167a1ecf8ef8355c9fa964f01319679e5bab
Dougie Squire <42455466+dougiesquire@users.noreply.github.com>
2024-08-05 15:31:33 +1000

    Update input directory name from "input" to "INPUT" (#20)

--- payu_config/setup_scripts/setup_cice_restarts.sh
+++ payu_config/setup_scripts/setup_cice_restarts.sh
@@ -3,9 +3,9 @@
 if ! [ -f work/access-om3.cice.r.* ]
 then 
 # no restart files yet, use initial conditions
-IC=$(readlink work/input/iced.1900-01-01-10800.nc)
-rm work/input/iced.1900-01-01-10800.nc
-cp $IC work/input/iced.1900-01-01-10800.nc
+IC=$(readlink work/INPUT/iced.1900-01-01-10800.nc)
+rm work/INPUT/iced.1900-01-01-10800.nc
+cp $IC work/INPUT/iced.1900-01-01-10800.nc
 else
 # change restart symlink to hardlink
 RESTART=$(echo work/access-om3.cice.r.*)
