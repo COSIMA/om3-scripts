a83c189b7ff1c4cc2ada35edd0bc5674596686d2
minghangli-uni <minghang.li1@anu.edu.au>
2025-01-28 09:11:41 +1100

    Remove experiment_manager_tool directory

--- expts_manager/experiment_manager_tool/configuration_updater/common.py
+++
@@ -1,9 +0,0 @@
-def update_config_entries(base: dict, change: dict) -> None:
-    """
-    Recursively update nuopc_runconfig and config.yaml entries.
-    """
-    for k, v in change.items():
-        if isinstance(v, dict) and k in base and isinstance(base[k], dict):
-            update_config_entries(base[k], v)
-        else:
-            base[k] = v
