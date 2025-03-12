511ca70bdccb02574f82aaca91860320f6e4b6b8
anton-seaice <anton.steketee@anu.edu.au>
2025-03-12 11:39:06 +1100

    use xp65 instead of hh5 for new versions

--- payu_config/archive.sh
+++ payu_config/archive.sh
@@ -2,4 +2,4 @@ source $(dirname "$0")/archive_scripts/archive_cice_restarts.sh
 source $(dirname "$0")/archive_scripts/concat_ice_daily.sh
 source $(dirname "$0")/archive_scripts/standardise_mom6_filenames.sh
 
-qsub -lstorage=gdata/hh5+${PBS_NCI_STORAGE} -v SCRIPTS_DIR=$(dirname "$0"),PROJECT $(dirname "$0")/archive_scripts/build_intake_ds.sh
+qsub -lstorage=gdata/xp65+${PBS_NCI_STORAGE} -v SCRIPTS_DIR=$(dirname "$0"),PROJECT $(dirname "$0")/archive_scripts/build_intake_ds.sh
